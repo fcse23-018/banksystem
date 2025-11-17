8fe2bafc6ed6b5a3b989159142c18c544a162396
thedonswiss <thedonswiss@gmail.com>
2025-11-17 11:39:45 +0000

    Changes before Firebase Studio auto-run

--- banksystem/src/main/java/com/bankingsystem/controller/TransactionDialogController.java
+++ banksystem/src/main/java/com/bankingsystem/controller/TransactionDialogController.java
@@ -6,6 +6,7 @@ import com.bankingsystem.model.Account;
 import com.bankingsystem.model.Transaction;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
